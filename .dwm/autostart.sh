cfc7937ded56296b1596ce921d641f9c5ad9d4f2
root <bigfreak@web.de>
2017-06-08 04:48:50 +0200

    Autostart update

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-fswebcam -S 5 -q --no-banner -r 1280x720 ~/Pictures/logon/$(date +%d-%m-%Y_%T%H).jpg
+#Audiobell
+pactl upload-sample /usr/share/sounds/freedesktop/stereo/bell.oga x11-bell &
+pactl load-module module-x11-bell sample=x11-bell display=$DISPLAY &
+lidstatus=$(cat /proc/acpi/button/lid/LID/state)
+if [[ $lidstatus == *"open"* ]]; then
+  fswebcam -S 5 -q --no-banner -r 1280x720 ~/Pictures/logon/$(date +%d-%m-%Y_%T%H).jpg
+fi
+mplayer "/home/bigfreak/Downloads/startupsounds/Sega Dreamcast - Boot.wav" </dev/null >/dev/null 2>&1 &
 nitrogen --restore & 
 xautolock -time 10 -locker /home/bigfreak/git/i3lock-fancy/lock -nowlocker /home/bigfreak/git/i3lock-fancy/lock -detectsleep -corners 000+ -cornerdelay 3 & 
 /home/bigfreak/Documents/scripts/dwm_status.sh & 
