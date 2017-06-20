9baf46b17744b300d11983fdbed61bdd095ad598
root <bigfreak@web.de>
2017-06-20 13:22:29 +0200

    pruefen ob Prozesse bereits laufen

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -17,14 +17,19 @@ mplayer "/home/bigfreak/Downloads/startupsounds/Sega Dreamcast - Boot.wav" </dev
 nitrogen --restore &
 
 #Lock-Screen when Mouse 3 Seconds on right corner (i3lock)
+if ! pgrep -x "xautolock" > /dev/null; then
 xautolock -time 10 -locker /home/bigfreak/git/i3lock-fancy/lock -nowlocker /home/bigfreak/git/i3lock-fancy/lock -detectsleep -corners 000+ -cornerdelay 3 & 
+fi
 
 #XServer Rootwindow used as Statusbar
+if ! pgrep -x "dwm_status.sh" > /dev/null; then
 /home/bigfreak/Documents/scripts/dwm_status.sh & 
-
+fi
 #Notify Daemon
+if ! pgrep -x "dunst" > /dev/null; then
 dunst & 
-
+fi
 #Clipoard-Manager
+if ! pgrep -x "clipmenud" > /dev/null; then
 /home/bigfreak/git/clipmenu/clipmenud & 
-
+fi
